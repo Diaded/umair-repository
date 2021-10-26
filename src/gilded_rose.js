a75aefdffb2d1fca88ef55d3122768da4c6ca4f2
Umair Khalil <khalilumair16@gmail.com>
2021-10-26 18:12:28 -0400

    test: created test for backstage passes exception

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,53 +18,13 @@ const qualityError = 'Quality is less then 0'
 
 function update_quality() {
 
-  for (var i = 0; i < items.length; i++) {
+  for (const item of items) {
     
-    if(!Number.isInteger(items[i].sell_in) || !Number.isInteger(items[i].quality)) throw typeError
-    if(items[i].quality < 0) throw qualityError
+    if(!Number.isInteger(item.sell_in) || !Number.isInteger(item.quality)) throw typeError
+    if(item.quality < 0) throw qualityError
 
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
+    
+
+
   }
 }
