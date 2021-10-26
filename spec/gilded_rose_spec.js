efb5450abcfe3cdd41e228c69d9d7e71a6e23a6a
Umair Khalil <khalilumair16@gmail.com>
2021-10-26 18:12:48 -0400

    test: created test for backstage passes exception

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -7,11 +7,26 @@ describe("Gilded Rose", function() {
         expect(update_quality).toThrow(typeError)
       })
 
-      it('if quality is less then 0 error is throw', () => {
+      it('if quality is less then 0 throw error', () => {
         items = []
         items.push(new Item('', 10, -1))
         expect(update_quality).toThrow(qualityError)
       })
+    })
+
+    describe('Dealing with back stage passes', () => {
+      it("quality of backstage passes should increase when sell in days get smaller", () => {
+        items = []
+        let sell_in = 25
+        let quality = 50
+        items.push(new Item('Backstage passes', sell_in, quality))
+
+        update_quality()
+
+        expect(items[0].quality).toEqual(51)
+
+      })
+
     })
 
 });
