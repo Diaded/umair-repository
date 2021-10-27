d82ee6cc9283c41d30814fc10b2188f05568a80f
Umair Khalil <khalilumair16@gmail.com>
2021-10-26 21:38:50 -0400

    refactor: aged brie quality should go up as sell in days go down - passed

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -34,6 +34,11 @@ function handle_sulfras(item) {
   item.sell_in -=1
 }
 
+function handle_aged_brie(item) {
+  item.sell_in -=1
+  item.quality +=1
+}
+
 function update_quality() {
 
   for (const item of items) {
@@ -43,6 +48,7 @@ function update_quality() {
 
     if(item.name.includes("Backstage passes"))  handle_backstage_passes(item)
     else if(item.name.includes('Sulfuras')) handle_sulfras(item)
+    else if(item.name === "Aged Brie") handle_aged_brie(item)
 
 
   }
